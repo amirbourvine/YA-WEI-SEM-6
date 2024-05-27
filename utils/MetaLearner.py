6ecb4defa9a3b21f9a568927a3d8ad42971ee308
amirbourvine <amirbourvine@gmail.com>
2024-05-28 00:20:00 +0300

    implemented weights and clusters based on similarity

--- utils/MetaLearner.py
+++ utils/MetaLearner.py
@@ -3,11 +3,11 @@ import torch
 
 
 import itertools
-def findMaxCombinations(tensor, evaluate, max_batch_size, n):
+def findMaxCombinations(tensor, evaluate, min_batch_size, max_batch_size, n):
     # Generate all possible not empty subsets
     all_subsets = []
     indices = range(tensor.shape[-1])
-    for r in range(1, max_batch_size + 1):
+    for r in range(min_batch_size, max_batch_size + 1):
         subsets = torch.IntTensor(list(itertools.combinations(indices, r)))
         all_subsets.extend(subsets)
     
@@ -15,9 +15,9 @@ def findMaxCombinations(tensor, evaluate, max_batch_size, n):
     evaluated_subsets = torch.FloatTensor([evaluate(torch.index_select(tensor, -1, subset)) for subset in all_subsets])
 
     # find the n largest evaluated subsets
-    _, largest_subsets_indices = torch.topk(evaluated_subsets, n)
+    values, largest_subsets_indices = torch.topk(evaluated_subsets, n)
 
-    return [all_subsets[largest_subsets_indices[i].item()] for i in range(largest_subsets_indices.shape[0])]
+    return  [values[i] for i in range(largest_subsets_indices.shape[0])] ,[all_subsets[largest_subsets_indices[i].item()] for i in range(largest_subsets_indices.shape[0])]
 
 class HDDOnBands:
     def run(tensor):
@@ -33,5 +33,8 @@ class HDDOnBands:
     def createL1WeightedBatches(tensor):
         return torch.sum(HDDOnBands.run(tensor), axis=1), [[i] for i in range(tensor.shape[-1])]
 
-    def createMaxCovarianceBasedBatches(tensor, max_batch_size = 3):
-        pass
+    def createMinSimilarityBasedBatches(tensor, n):
+        def evaluate(ten):
+            return torch.norm(ten[:,:,0].float()-ten[:,:,1].float(), p=1)
+        
+        return findMaxCombinations(tensor, evaluate, 2, 2, n)
