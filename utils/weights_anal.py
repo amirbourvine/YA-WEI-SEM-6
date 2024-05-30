7866cbceb3906513370373b3d623abb5c99c6f20
amirbourvine <amirbourvine@gmail.com>
2024-05-30 18:21:58 +0300

    weights analysis

--- utils/weights_anal.py
+++ utils/weights_anal.py
@@ -16,9 +16,22 @@ def show_weights(weights, is_normalized=True):
         plt.ylim((0,1))
     plt.show()
 
-def find_levels(weights):
-    levels = np.log(weights)
-    return levels/(np.min(levels))
+
+def show_structure(d_HDD):
+  embedding = MDS(n_components=2, normalized_stress='auto', dissimilarity='precomputed')
+  X_transformed = embedding.fit_transform(d_HDD)
+
+  levels = np.zeros(X_transformed.shape[0])
+  for i in range(levels.shape[0]):
+    levels[i] = (i+1).bit_length()
+
+  cmap = cm.Set1
+  norm = colors.BoundaryNorm(np.arange(0.5, 6, 1), cmap.N)
+
+  plt.scatter(X_transformed[:, 0], X_transformed[:, 1],c=levels, norm=norm, cmap = cmap, edgecolors='black',s=100)
+  cbar = plt.colorbar(ticks=np.linspace(1, 5, 5))
+  cbar.set_label('level')
+  plt.show()
 
 
 
