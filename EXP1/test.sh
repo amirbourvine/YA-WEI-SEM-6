6eee3e78a92855b0ec4ff373445d3ea63883a2c2
amirbourvine <amirbourvine@gmail.com>
2024-06-27 16:42:31 +0300

    updates

---
+++ EXP1/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=ya-wei_job
+#SBATCH -p high
+#SBATCH --array=0-7
+#SBATCH -N 1
+#SBATCH --gpus=2
+#SBATCH --cpus-per-gpu=2
+#SBATCH --time=2-00:00:00
+#SBATCH --mem-per-gpu=64G
+#SBATCH --output=my_job_%A_%a.out
+#SBATCH --error=my_job_%A_%a.err
+
+
+
+# Run the task
+python3 wasser_classify.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
